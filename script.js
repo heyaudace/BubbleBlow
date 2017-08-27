193b8f5649f8884bb58c81714f9359652ce6bb39
Audace Nakeshimana <audnakeshimana@gmail.com>
2017-08-27 00:03:36 -0700

    styling

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
-function giveBubble(index, maximum_diameter){
-	var diameter = Math.ceil((Math.random()*maximum_diameter*0.1));
-	var diameter_element = '<div class="diameter_element">'+diameter.toString()+'</div>';
+function giveBubble(index, maximum_diameter) {
+	var diameter = Math.ceil((Math.random() * maximum_diameter * 0.1));
+	var diameter_element = '<div class="diameter_element">' + diameter.toString() + '</div>';
 
-	var colors = ["blue", "yellow", "orange", "red", "green", "#337ab7"];
-	var color = colors[Math.floor(Math.random()*colors.length)];
+	var colors = ['blue', 'yellow', 'orange', 'red', 'green', '#337ab7'];
+	var color = colors[Math.floor(Math.random() * colors.length)];
 	var max_score_computer = parseInt($('#max_score_computer').text());
-	max_score_computer += diameter
+	max_score_computer += diameter;
 
 	$('#max_score_computer').text(max_score_computer);
 
 	var current_score_now = parseInt($('#current_score_computer').text());
-	var efficiency_now = (current_score_now/max_score_computer)*100
+	var efficiency_now = (current_score_now / max_score_computer) * 100;
 
 	$('#efficiency_computer').text(efficiency_now.toString().substring(0, 5));
 
-	var magin_number = Math.floor(Math.random()*80);
-	new_css = 'width: '+diameter.toString()+'px; height: '+diameter.toString()+ 
-	'px; background-color: '+color+'; points='+diameter.toString()+'; margin-left: '+magin_number.toString()+'%';
-	var circle = '<div id="bubble'+index.toString()+'" class="bubbles" style="'+new_css+'" index="'+index.toString()+'" points="'+diameter+'">'+diameter_element+'</div>';
-	return circle
+	var magin_number = Math.floor(Math.random() * 80);
+	new_css = 'width: ' + diameter.toString() + 'px; height: ' + diameter.toString() +
+	'px; background-color: ' + color + '; points=' + diameter.toString() + '; margin-left: ' + magin_number.toString() + '%';
+	var circle = '<div id="bubble' + index.toString() + '" class="bubbles" style="' + new_css + '" index="' + index.toString() + '" points="' + diameter + '">' + diameter_element + '</div>';
+	return circle;
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$('div').dblclick(function(e){
+	$('div').dblclick(function(e) {
 		var this_class = e.target.getAttribute('class');
-		if(this_class == 'bubbles'){
+		if (this_class == 'bubbles') {
 			this_id = e.target.getAttribute('id');
-			$('#'+this_id).css('border', '5px solid gray');
-			$('#'+this_id).effect('explode')
+			$('#' + this_id).css('border', '5px solid gray');
+			$('#' + this_id).effect('explode');
 		}
 	});
-	$('div').click(function(e){
+	$('div').click(function(e) {
 		var this_class = e.target.getAttribute('class');
-		if(this_class == 'bubbles'){
+		if (this_class == 'bubbles') {
 			this_id = e.target.getAttribute('id');
-			$('#'+this_id).addClass('blownbubbles').removeClass('bubbles');
+			$('#' + this_id).addClass('blownbubbles').removeClass('bubbles');
 			var these_points = parseInt(e.target.getAttribute('points'));
-			var current_points = parseInt($('#current_score_computer').text())
-			current_points += these_points
+			var current_points = parseInt($('#current_score_computer').text());
+			current_points += these_points;
 			$('#current_score_computer').text(current_points);
-			$('#'+this_id).effect('bounce', {times: 5}, "fast");
-			$('#current_score_computer').effect('bounce', {times: 20}, "slow");
-			$('#'+this_id).effect('explode', 500);
+			$('#' + this_id).effect('bounce', {times: 5}, 'fast');
+			$('#current_score_computer').effect('bounce', {times: 20}, 'slow');
+			$('#' + this_id).effect('explode', 500);
 		}
 	});
 
 	var timestep = 0;
-	function generate(){
-		if(timestep < 1000){
-			mycircle = giveBubble(timestep, screen.width*.8)
+	function generate() {
+		if (timestep < 1000) {
+			mycircle = giveBubble(timestep, screen.width * .8);
 			$('#playground_border').prepend(mycircle);
-			$('div').each(function(){
+			$('div').each(function() {
 				var this_class = $(this).attr('class');
-				if(this_class == 'bubbles'){
+				if (this_class == 'bubbles') {
 					this_index = parseInt($(this).attr('index'));
-					if(this_index < timestep - 10){
+					if (this_index < timestep - 10) {
 						$(this).css('background-color', 'yellow');
 						$(this).fadeOut(200);
 					}
 				}
 
-			})
-			timestep++
-			animate = setTimeout(generate, 500)
+			});
+			timestep++;
+			animate = setTimeout(generate, 500);
 		}
-		else{
-			clearTimeout(animate)
+		else {
+			clearTimeout(animate);
 		}
 	}
 	generate();
-})
+});
